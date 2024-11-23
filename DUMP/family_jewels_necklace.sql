7622b0ce270897f477b2d3b3c077f84cdb492690
Henry-H-W <hwan577@uwo.ca>
2024-11-22 19:46:37 -0500

    updated nacklace DUMP

--- DUMP/family_jewels_necklace.sql
+++ DUMP/family_jewels_necklace.sql
@@ -25,14 +25,14 @@ DROP TABLE IF EXISTS `necklace`;
 CREATE TABLE `necklace` (
   `necklaceId` int NOT NULL AUTO_INCREMENT,
   `linkId` int NOT NULL,
-  `name` varchar(30) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
   `linkAmount` int DEFAULT '20',
   `size` float DEFAULT NULL,
   `totalVolume` float DEFAULT NULL,
   PRIMARY KEY (`necklaceId`),
   KEY `linkId` (`linkId`),
   CONSTRAINT `necklace_ibfk_1` FOREIGN KEY (`linkId`) REFERENCES `link` (`linkId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +41,7 @@ CREATE TABLE `necklace` (
 
 LOCK TABLES `necklace` WRITE;
 /*!40000 ALTER TABLE `necklace` DISABLE KEYS */;
+INSERT INTO `necklace` VALUES (1,1,'Cuban Link Necklace, 315mm',30,315,369),(2,1,'Cuban Link Necklace, 367.5mm',35,367.5,430.5),(3,1,'Cuban Link Necklace, 420mm',40,420,492),(4,1,'Cuban Link Necklace, 472.5mm',45,472.5,553.5),(5,1,'Cuban Link Necklace, 525mm',50,525,615),(6,2,'Cuban Link Necklace, 330mm',30,330,390),(7,2,'Cuban Link Necklace, 385mm',35,385,455),(8,2,'Cuban Link Necklace, 440mm',40,440,520),(9,7,'Curb Chain Necklace, 255mm',30,255,300),(10,8,'Curb Chain Necklace, 240mm',30,240,285),(11,10,'Curb Chain Necklace, 264.0000057220459mm',30,264,306),(12,11,'Figaro Chain Necklace, 270mm',30,270,303),(13,12,'Figaro Chain Necklace, 279mm',30,279,315),(14,12,'Figaro Chain Necklace, 325.5mm',35,325.5,367.5),(15,12,'Figaro Chain Necklace, 372mm',40,372,420),(16,12,'Figaro Chain Necklace, 418.5mm',45,418.5,472.5),(17,51,'Singapore Chain Necklace, 315mm',45,315,369),(18,54,'Singapore Chain Necklace, 324mm',45,324,373.5),(19,55,'Singapore Chain Necklace, 337.5mm',45,337.5,391.5),(20,66,'Bar Chain Necklace, 279mm',45,279,337.5),(21,67,'Bar Chain Necklace, 292.5mm',45,292.5,351),(22,68,'Bar Chain Necklace, 270mm',45,270,328.5);
 /*!40000 ALTER TABLE `necklace` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-22 19:12:58
+-- Dump completed on 2024-11-22 19:46:06
